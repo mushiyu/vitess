d3e98381ba07ef02744745152bc4326b11239266
mushiyu <935261221@qq.com>
2023-01-19 13:35:19 +0800

    first commit

---
+++ go/cmd/vttablet/plugin_opentsdb.go
@@ -0,0 +1,11 @@
+package main
+
+// This plugin imports opentsdb to register the opentsdb stats backend.
+
+import (
+	"github.com/mushiyu/vitess/go/stats/opentsdb"
+)
+
+func init() {
+	opentsdb.Init("vttablet")
+}
